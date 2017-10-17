09b84e5a11d1f64d31618fe062fdf02a3c08c77f
kens100 <kens100@163.com>
2017-10-18 02:57:16 +0800

    更新users测试例 修改login欢迎字

--- apps/users/tests.py
+++ apps/users/tests.py
@@ -1,3 +1,34 @@
+# _*_ encoding:utf-8 _*_
 from django.test import TestCase
+from django.test import Client
+
+from users.models import UserProfile
 
 # Create your tests here.
+class LoginViewTestCase(TestCase):
+    def setUp(self):
+        UserProfile.objects.create_user("anna", "anna1234@163.com", "anna1236")
+
+    def test_login_all_right(self):
+        c = Client()
+        response = c.post('/login/', {'username': 'anna', 'password': 'anna1234'})
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "success")
+
+    def test_login_password_wrong(self):
+        c = Client()
+        response = c.post('/login/', {'username': 'anna', 'password': 'anna3456'})
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "fail")
+
+    def test_login_username_wrong(self):
+        c = Client()
+        response = c.post('/login/', {'username': 'benny', 'password': 'anna1234'})
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "fail")
+
+    def test_login_all_wrong(self):
+        c = Client()
+        response = c.post('/login/', {'username': 'benny', 'password': 'anna3456'})
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "fail")
