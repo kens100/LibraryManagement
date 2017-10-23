1f0f1feae23fa0fe02fee74a1aaf40f282f9281c
Goldeave <1658724684@qq.com>
2017-10-23 20:19:40 +0800

    修改book.view

--- apps/book/views.py
+++ apps/book/views.py
@@ -66,40 +66,6 @@ class CheckView(View):
         # return HttpResponse({"status": "nothing"}, content_type='application/json')
 
 
-class EnStoreView(View):
-    def get(self, request):
-        if not request.user.is_authenticated():
-            return render(request, "login.html")
-        return render(request, "enStore.html")
-
-    def post(self, request):
-        if not request.user.is_authenticated():
-            return render(request, "login.html")
-        book = request.POST.get("book", "")
-        press = request.POST.get("press", "")
-        count = request.POST.get("count", 0)
-        target_press = Press.objects.filter(name=press)
-        if not target_press:
-            return HttpResponse('{"status":"fail","msg":"入库失败，不存在该出版社"}', content_type='application/json')
-        target_press = target_press[0]
-        press_id = target_press.id
-        oldBook = Book.objects.filter(name=book, press=press_id)
-        if not oldBook:
-            return HttpResponse('{"status":"fail","msg":"入库失败，请先添加该书籍，再入库"}', content_type='application/json')
-        # oldBook = oldBook[0]
-        # if oldBook.max_count is not None and oldBook.max_count < (oldBook.count + int(count)):
-        #     return HttpResponse('{"status":"fail","msg":"入库失败，书籍库存大于该书籍允许的最大库存"}', content_type='application/json')
-        oldBook.max_amount = oldBook.max_amount + int(count)
-        oldBook.save()
-        enStoreRecord = EnStore()
-        enStoreRecord.count = int(count)
-        enStoreRecord.manager = request.user
-        enStoreRecord.book = oldBook
-        enStoreRecord.press = target_press
-        enStoreRecord.save()
-        return HttpResponse('{"status":"success","msg":"入库成功"}', content_type='application/json')
-
-
 class OutStoreView(View):
     def get(self, request):
         if not request.user.is_authenticated():
