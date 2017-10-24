53fe5e71642fb1ac6c4cd8b78cc4b80a04ce943f
kens100 <kens100@163.com>
2017-10-24 16:58:14 +0800

    修改Borrow类字段

--- apps/borrow/models.py
+++ apps/borrow/models.py
@@ -13,7 +13,7 @@ class Borrow(models.Model):
     book = models.ForeignKey(Book, verbose_name=u"书籍")
     proof = models.ForeignKey(Proof, verbose_name=u"借阅者")
     borrow_time = models.DateTimeField(default=datetime.now, verbose_name=u"借阅时间")
-    return_time = models.DateTimeField(default=0, verbose_name=u"还书时间")
+    return_time = models.DateTimeField(default=None, null=True, blank=True, verbose_name=u"还书时间")
 
     class Meta:
         verbose_name = u"借阅记录"
