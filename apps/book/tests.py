16acce906ae7d90ca7888369ab31dd7cdb2941ba
shengdaididi <32241468+shengdaididi@users.noreply.github.com>
2017-10-25 19:18:24 +0800

    增加借阅测试类

--- apps/book/tests.py
+++ apps/book/tests.py
@@ -84,3 +84,51 @@ class OutStoreViewTestCase(TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, "fail")
         self.assertContains(response, "出库失败，书籍库存小于你要的取货量")
+
+#测试借阅
+class OnBorrowViewTestCase(TestCase):
+    def setUp(self):
+        UserProfile.objects.create_user("anna", "anna1234@163.com", "anna1234")
+        press_test = Press.objects.create(name=u'Press Test', phone='12345678901', address=u'Address Test',
+                                          contact=u'Contact Test')
+        book_test = Book.objects.create(name=u'Book Test', writer=u'Writer Test', press=press_test, price=30,
+                                        max_amount=1)
+        proof_test = Proof.objects.create(name=u'testProof', address=u'AddressTest', id_number='12345678', phone='1234567890')
+        self.test_login_all_right()
+
+    def test_login_all_right(self):
+        response = self.client.post('/login/', {'username': 'anna', 'password': 'anna1234'})
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "success")
+
+    def test_onBorrow_all_right(self):
+        response = self.client.post('/onBorrow/', {'book': u'Book Test', 'press': u'Press Test', 'proof':u'testProof'})
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "success")
+
+    def test_onBorrow_proof_name_wrong(self):
+        response = self.client.post('/onBorrow/', {'book': u'Book Test', 'press': u'Press Test','proof': u'testProof1'})
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "fail")
+        self.assertContains(response, "查询失败，不存在该借阅者")
+
+    def test_onBorrow_book_press_wrong(self):
+        response = self.client.post('/onBorrow/', {'book': u'Book Test', 'press': u'Press Test1', 'proof': u'testProof'})
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "fail")
+        self.assertContains(response, "查询失败，不存在该出版社")
+
+    def test_onBorrow_book_name_wrong(self):
+        response = self.client.post('/onBorrow/', {'book': u'Book Test1', 'press': u'Press Test', 'proof': u'testProof'})
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "fail")
+        self.assertContains(response, "查询失败，仓库没有该书籍")
+
+    def test_onBorrow_book_count_wrong(self):
+        response = self.client.post('/onBorrow/', {'book': u'Book Test', 'press': u'Press Test', 'proof': u'testProof'})
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "success")
+        response = self.client.post('/onBorrow/', {'book': u'Book Test', 'press': u'Press Test', 'proof': u'testProof'})
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "fail")
+        self.assertContains(response, "借阅失败，书籍库存小于你要的取货量")
