2927d497c76db85841b24e851157040e95fcedd9
Sia <32241685+sia1588@users.noreply.github.com>
2017-10-25 23:07:33 +0800

    修改测试函数名称

--- apps/proof/tests.py
+++ apps/proof/tests.py
@@ -7,7 +7,7 @@ from proof.models import Proof
 
 
 # Create your tests here.
-class AddPressViewTestCase(TestCase):
+class AddProofViewTestCase(TestCase):
     def setUp(self):
         UserProfile.objects.create_user("anna", "anna1234@163.com", "anna1234")
         Proof.objects.create(name=u'Proof Test1', address=u'Address Test',
@@ -19,41 +19,41 @@ class AddPressViewTestCase(TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, "success")
 
-    def test_add_press_all_right(self):
+    def test_add_proof_all_right(self):
         response = self.client.post('/addProof/', {'proof': u'Proof Test', 'address': u'Address Test',
                                                    'id_number': u'3117001235', 'phone': u'1234567890'})
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, "success")
 
-    def test_add_press_no_idnumber(self):
+    def test_add_proof_no_idnumber(self):
         response = self.client.post('/addProof/', {'proof': u'Proof Test', 'address': u'Address Test',
                                                    'phone': u'1234567890'})
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, "fail")
         self.assertContains(response, "添加失败，请输入ID号码")
 
-    def test_add_press_idnumber_wrong(self):
+    def test_add_proof_idnumber_wrong(self):
         response = self.client.post('/addProof/', {'proof': u'Proof Test', 'address': u'Address Test',
                                                    'id_number': u'3117001234', 'phone': u'1234567890'})
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, "fail")
         self.assertContains(response, "添加失败，已存在该ID号码")
 
-    def test_add_press_no_name(self):
+    def test_add_proof_no_name(self):
         response = self.client.post('/addProof/', {'address': u'Address Test',
                                                    'id_number': u'3117001235', 'phone': u'1234567890'})
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, "fail")
         self.assertContains(response, "添加失败，请输入借阅者名称")
 
-    def test_add_press_no_address(self):
+    def test_add_proof_no_address(self):
         response = self.client.post('/addProof/', {'proof': u'Proof Test',
                                                    'id_number': u'3117001235', 'phone': u'1234567890'})
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, "fail")
         self.assertContains(response, "添加失败，请输入客户地址")
 
-    def test_add_press_no_phone(self):
+    def test_add_proof_no_phone(self):
         response = self.client.post('/addProof/', {'proof': u'Proof Test', 'address': u'Address Test',
                                                    'id_number': u'3117001235'})
         self.assertEqual(response.status_code, 200)
