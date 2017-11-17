243dba64c72277e9c706d2f16a95ab90165c8690
kens100 <kens100@163.com>
2017-11-17 13:09:59 +0800

    修改借阅者类

--- apps/proof/models.py
+++ apps/proof/models.py
@@ -8,11 +8,11 @@ from datetime import datetime
 
 
 class Proof(models.Model):
+    id = models.CharField(max_length=50, verbose_name=u"ID号码", primary_key=True)
     name = models.CharField(max_length=50,verbose_name=u"借阅者名称")
     sex = models.IntegerField(choices=((1, "男"), (2, "女")), default=1, verbose_name=u"性别")
     birthday = models.DateTimeField(default=datetime.now, verbose_name=u"生日")
     address = models.CharField(max_length=100,verbose_name=u"客户地址")
-    id_number = models.CharField(max_length=50, verbose_name=u"ID号码")
     phone = models.CharField(max_length=15,verbose_name=u"联系电话")
     now_borrow_amount = models.IntegerField(default=0, verbose_name=u"借阅数")
     add_time = models.DateTimeField(default=datetime.now, verbose_name=u"创建日期")
